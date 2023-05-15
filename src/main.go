ea404cae0b662c720cabc57127fdf88a5eaa08ce
James Gasek <jmg224@lehigh.edu>
2023-05-15 12:32:24 -0400

    initial commit

--- src/main.go
+++ src/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	//gin
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	api_uri := "/api/v1"
+
+	router := gin.Default()
+
+	router.GET(api_uri, func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+
+	})
+
+	router.Run(":8080")
+
+	fmt.Println("Server running on port 8080")
+
+	//router.Run()
+
 }
